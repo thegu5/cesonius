c00e027bd0a29c7c5dfb924d1f7571492f5d4c02
thegu5 <thegu5@github.com>
2020-07-24 21:17:13 -0400

    update

--- scripts/controller.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-class_name Controller
-
-# Get the reference to a character
-# All controllers, like AI or a player inherit parameters of this class
-onready var character : Character = get_parent()
-
-func _ready():
-	pass
--- scripts/peer.gd
+++
@@ -1,9 +0,0 @@
-extends Controller
-class_name Peer
-
-# Peer is a representation of other players
-# We can't controll this node
-
-# For type checking
-func is_peer():
-	return true
